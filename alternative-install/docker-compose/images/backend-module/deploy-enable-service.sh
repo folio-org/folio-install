dc18990fff7f285b2ac8d7213fe27e2dd759b2fe
Mark Stacy <mbstacy@gmail.com>
2018-10-23 23:37:31 -0600

    backend module container builder for deploy and enable okapi

---
+++ alternative-install/docker-compose/images/backend-module/deploy-enable-service.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DEPLOY_DESCRIPTOR={\"srvcId\":\""$MODULE"-"$TAG"\",\"instId\":\""$MODULE"-"$TAG"\",\"url\":\""$MODULE_URL"\"}
+HTTPD=$(curl -X POST -H 'Content-type: application/json' -sL -d "$DEPLOY_DESCRIPTOR" --connect-timeout 3 -w "%{http_code}\n" "$OKAPI_URL/_/discovery/modules" -o /dev/null);
+echo "$HTTPD";
+until [ "$HTTPD" == "200" ]; do
+    printf '.'
+    sleep 3
+    HTTPD=$(curl -X POST -H 'Content-type: application/json' -sL -d "$DEPLOY_DESCRIPTOR" --connect-timeout 3 -w "%{http_code}\n" "$OKAPI_URL/_/discovery/modules" -o /dev/null);
+done;
+
+ENABLE_MODULE={\"id\":\""$MODULE"-"$TAG"\"}
+HTTPD=$(curl -X POST -H 'Content-type: application/json' -sL -d "$ENABLE_MODULE" --connect-timeout 3 -w "%{http_code}\n" "$OKAPI_URL/_/proxy/tenants/$OKAPI_TENANT/modules" -o /dev/null);
+until [ "$HTTPD" == "200" ]; do
+    printf '.'
+    sleep 3
+    HTTPD=$(curl -X POST -H 'Content-type: application/json' -sL -d "$ENABLE_MODULE" --connect-timeout 3 -w "%{http_code}\n" "$OKAPI_URL/_/proxy/tenants/$OKAPI_TENANT/modules" -o /dev/null);
+done;
+
+./run-java.sh
