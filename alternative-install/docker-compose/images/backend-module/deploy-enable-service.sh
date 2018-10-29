97d637f6661a756dc55863979cd5f85a5be6b824
Mark Stacy <mbstacy@gmail.com>
2018-10-28 20:22:18 -0600

    backend module image

--- alternative-install/docker-compose/images/backend-module/deploy-enable-service.sh
+++ alternative-install/docker-compose/images/backend-module/deploy-enable-service.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-DEPLOY_DESCRIPTOR={\"srvcId\":\""$MODULE"-"$TAG"\",\"instId\":\""$MODULE"-"$TAG"\",\"url\":\""$MODULE_URL"\"}
-HTTPD=$(curl -X POST -H 'Content-type: application/json' -sL -d "$DEPLOY_DESCRIPTOR" --connect-timeout 3 -w "%{http_code}\n" "$OKAPI_URL/_/discovery/modules" -o /dev/null);
-echo "$HTTPD";
-until [ "$HTTPD" -lt "400" ]; do
-    printf '.'
-    sleep 3
+#Allow a little time for okapi to come up. Not sure if needed but keeping in for now.
+sleep 100;
+#Check if module already registered. Important when you are not initializing DB
+HTTPD=$(curl -X GET -H 'Content-type: application/json' -sL --connect-timeout 10 -w "%{http_code}\n" "$OKAPI_URL/_/discovery/modules/$MODULE-$TAG" -o /dev/null);
+if [ "$HTTPD" -ge "400" ]; then
+    #Register discovery modules
+    DEPLOY_DESCRIPTOR={\"srvcId\":\""$MODULE"-"$TAG"\",\"instId\":\""$MODULE"-"$TAG"\",\"url\":\""$MODULE_URL"\"}
     HTTPD=$(curl -X POST -H 'Content-type: application/json' -sL -d "$DEPLOY_DESCRIPTOR" --connect-timeout 3 -w "%{http_code}\n" "$OKAPI_URL/_/discovery/modules" -o /dev/null);
-done;
+    echo "$HTTPD";
+    until [ "$HTTPD" -lt "400" ]; do
+        printf '.'
+        sleep 3
+        HTTPD=$(curl -X POST -H 'Content-type: application/json' -sL -d "$DEPLOY_DESCRIPTOR" --connect-timeout 3 -w "%{http_code}\n" "$OKAPI_URL/_/discovery/modules" -o /dev/null);
+    done;
+fi
 
-ENABLE_MODULE={\"id\":\""$MODULE"-"$TAG"\"}
-HTTPD=$(curl -X POST -H 'Content-type: application/json' -sL -d "$ENABLE_MODULE" --connect-timeout 3 -w "%{http_code}\n" "$OKAPI_URL/_/proxy/tenants/$OKAPI_TENANT/modules" -o /dev/null);
-until [ "$HTTPD" -lt "400" ]; do
-    printf '.'
-    sleep 3
-    HTTPD=$(curl -X POST -H 'Content-type: application/json' -sL -d "$ENABLE_MODULE" --connect-timeout 3 -w "%{http_code}\n" "$OKAPI_URL/_/proxy/tenants/$OKAPI_TENANT/modules" -o /dev/null);
-done;
-
-./run-java.sh
+(eval "$START_CMD");
