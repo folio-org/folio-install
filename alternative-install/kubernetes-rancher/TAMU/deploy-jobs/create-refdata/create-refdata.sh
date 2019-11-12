017b0f2f101045c8289768f8c6b7553337891ff3
ArgonV <33498203+ArgonV@users.noreply.github.com>
2019-11-12 13:53:40 -0600

    Updated repo for Q3.2
    Removed old, unused scripts and files, updated with Q3.2

--- alternative-install/kubernetes-rancher/TAMU/deploy-jobs/create-refdata/create-refdata.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-#Load Reference Data
-perl ./load-data.pl --tenant $TENANT_ID --user $ADMIN_USER --password $ADMIN_PASSWORD --okapi $OKAPI_URL --sort location-units/institutions,location-units/campuses,location-units/libraries,locations,statistical-code-types --custom-method loan-rules-storage=PUT /reference-data
