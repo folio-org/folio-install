e08392923744879df3db798c4d349b8bd9d1f346
Jason Root <33498203+ArgonV@users.noreply.github.com>
2019-11-12 13:18:03 -0600

    Update stripes.config.js

--- alternative-install/kubernetes-rancher/TAMU/stripes-diku/stripes.config.js
+++ alternative-install/kubernetes-rancher/TAMU/stripes-diku/stripes.config.js
@@ -1,19 +1,49 @@
-const platformCore = require('@folio/platform-core/stripes.config.js');
-const { merge } = require('lodash');
-
-const platformComplete = {
+module.exports = {
+  okapi: { 'url':'http://localhost:9130', 'tenant':'diku' },
+  config: {
+    logCategories: 'core,path,action,xhr',
+    logPrefix: '--',
+    showPerms: false
+  },
   modules: {
-    '@folio/agreements' : {},
+    '@folio/acquisition-units': {},
+    '@folio/agreements': {},
     '@folio/calendar' : {},
+    '@folio/checkin' : {},
+    '@folio/checkout' : {},
+    '@folio/circulation' : {},
     '@folio/data-import' : {},
+    '@folio/developer' : {},
     '@folio/eholdings' : {},
-    '@folio/erm-usage' : {},
+    '@folio/erm-usage': {},
     '@folio/finance' : {},
-    '@folio/licenses' : {},
-    '@folio/marccat' : {},
-    '@folio/plugin-find-vendor' : {},
-    '@folio/orders' : {},
-    '@folio/vendors' : {}
+    '@folio/inventory' : {},
+    '@folio/invoice': {},
+    '@folio/licenses': {},
+    '@folio/local-kb-admin': {},
+    '@folio/myprofile' : {},
+    '@folio/notes' : {},
+    '@folio/orders': {},
+    '@folio/organizations' : {},
+    '@folio/plugin-create-item': {},
+    '@folio/plugin-find-agreement': {},
+    '@folio/plugin-find-contact': {},
+    '@folio/plugin-find-erm-usage-data-provider': {},
+    '@folio/plugin-find-instance' : {},
+    '@folio/plugin-find-interface' : {},
+    '@folio/plugin-find-license': {},
+    '@folio/plugin-find-po-line': {},
+    '@folio/plugin-find-user' : {},
+    '@folio/plugin-find-organization': {},
+    '@folio/plugin-find-po-line': {},
+    '@folio/requests' : {},
+    '@folio/search' : {},
+    '@folio/servicepoints' : {},
+    '@folio/stripes-acq-components' : {},
+    '@folio/stripes-erm-components': {},
+    '@folio/tags': {},
+    '@folio/tenant-settings' : {},
+    '@folio/users' : {}
   },
   branding: {
     logo: {
@@ -25,5 +55,3 @@ const platformComplete = {
     },
   }
 };
-
-module.exports = merge({}, platformCore, platformComplete);
