45972358046cd3781df1d4695ef567a2eb29f662
Jason Root <33498203+ArgonV@users.noreply.github.com>
2020-12-17 15:57:15 -0600

    Update startup.sh
    New hazelcast startup params added.

--- alternative-install/kubernetes-rancher/TAMU/okapi/startup.sh
+++ alternative-install/kubernetes-rancher/TAMU/okapi/startup.sh
@@ -15,6 +15,7 @@ if [ $INITDB = 'true' ]; then
     -Dpostgres_password=$PG_PASSWORD -Dpostgres_database=$PG_DATABASE -Dhost=$OKAPI_HOST -Dport=$OKAPI_PORT -Dokapiurl=$OKAPI_URL \
     -Dnodename=$OKAPI_NODENAME -Dloglevel=$OKAPI_LOGLEVEL \
     -Dhazelcast.ip=$HAZELCAST_IP -Dhazelcast.port=$HAZELCAST_PORT \
+    --add-modules java.se --add-exports java.base/jdk.internal.ref=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/sun.nio.ch=ALL-UNNAMED --add-opens java.management/sun.management=ALL-UNNAMED --add-opens jdk.management/com.sun.management.internal=ALL-UNNAMED \
     -jar okapi/okapi-core/target/okapi-core-fat.jar $OKAPI_COMMAND -cluster-host $OKAPI_CLUSTERHOST -cluster-port $HAZELCAST_VERTX_PORT -hazelcast-config-file $HAZELCAST_FILE
 
 else
@@ -26,5 +27,7 @@ else
     -Dpostgres_password=$PG_PASSWORD -Dpostgres_database=$PG_DATABASE -Dhost=$OKAPI_HOST -Dport=$OKAPI_PORT -Dokapiurl=$OKAPI_URL \
     -Dnodename=$OKAPI_NODENAME -Dloglevel=$OKAPI_LOGLEVEL \
     -Dhazelcast.ip=$HAZELCAST_IP -Dhazelcast.port=$HAZELCAST_PORT \
+    --add-modules java.se --add-exports java.base/jdk.internal.ref=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/sun.nio.ch=ALL-UNNAMED --add-opens java.management/sun.management=ALL-UNNAMED --add-opens jdk.management/com.sun.management.internal=ALL-UNNAMED \
     -jar okapi/okapi-core/target/okapi-core-fat.jar $OKAPI_COMMAND -cluster-host $OKAPI_CLUSTERHOST -cluster-port $HAZELCAST_VERTX_PORT -hazelcast-config-file $HAZELCAST_FILE
+
 fi
