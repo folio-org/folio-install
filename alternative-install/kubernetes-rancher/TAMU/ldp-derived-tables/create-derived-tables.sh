a4be38e5fd624fe1dacf2f421f46b10bcb71b231
Jason Root <33498203+ArgonV@users.noreply.github.com>
2022-03-23 15:10:55 -0500

    Update create-derived-tables.sh

--- alternative-install/kubernetes-rancher/TAMU/ldp-derived-tables/create-derived-tables.sh
+++ alternative-install/kubernetes-rancher/TAMU/ldp-derived-tables/create-derived-tables.sh
@@ -3,12 +3,16 @@
 # Set postgres password environment variable
 export PGPASSWORD=$PG_PASSWORD
 
+echo Setting schema access for reporting user.
+
 # Alter schema for ldpreport user
 psql -U $PG_USER -h $DB_HOST -d $PG_DATABASE -w --command "GRANT SELECT ON ALL TABLES IN SCHEMA public TO ldpreport;" &&\
 psql -U $PG_USER -h $DB_HOST -d $PG_DATABASE -w --command "ALTER SCHEMA folio_reporting OWNER TO ldpadmin;"
 
 # Change directories to run derived table code
 cd /usr/local/bin/ldp/sql/derived_tables
+
+echo Generating derived tables...
 
 # Queries can now be executed
 echo > logfile
@@ -21,9 +25,37 @@ for f in $( cat runlist.txt ); do
     psql -U $PG_USER -h $DB_HOST -d $PG_DATABASE -a -f tmpfile >> logfile 2>&1
 done
 
-# Set postgres password environment variable for postgres
-export PGPASSWORD=$PG_ROOT_PASSWORD
+echo Setting schema access for all other users.
+
+# After all database updates and derived table queries have completed, correct schema access and roles
+psql -U ldpadmin -h $DB_HOST -d $PG_DATABASE -w --command "GRANT USAGE ON SCHEMA mis TO ro_ldp_user;" &&\
+psql -U ldpadmin -h $DB_HOST -d $PG_DATABASE -w --command "GRANT USAGE ON SCHEMA folio_reporting TO ro_ldp_user;" &&\
+psql -U ldpadmin -h $DB_HOST -d $PG_DATABASE -w --command "GRANT USAGE ON SCHEMA public TO ro_ldp_user;" &&\
+psql -U ldpadmin -h $DB_HOST -d $PG_DATABASE -w --command "GRANT USAGE ON SCHEMA history TO ro_ldp_user;" &&\
+psql -U ldpadmin -h $DB_HOST -d $PG_DATABASE -w --command "GRANT SELECT ON ALL TABLES IN SCHEMA mis TO ro_ldp_user;" &&\
+psql -U ldpadmin -h $DB_HOST -d $PG_DATABASE -w --command "GRANT SELECT ON ALL TABLES IN SCHEMA folio_reporting TO ro_ldp_user;" &&\
+psql -U ldpadmin -h $DB_HOST -d $PG_DATABASE -w --command "GRANT SELECT ON ALL TABLES IN SCHEMA public TO ro_ldp_user;" &&\
+psql -U ldpadmin -h $DB_HOST -d $PG_DATABASE -w --command "GRANT SELECT ON ALL TABLES IN SCHEMA history TO ro_ldp_user;" &&\
+psql -U ldpadmin -h $DB_HOST -d $PG_DATABASE -w --command "ALTER DEFAULT PRIVILEGES IN SCHEMA mis GRANT SELECT ON TABLES TO ro_ldp_user;" &&\
+psql -U ldpadmin -h $DB_HOST -d $PG_DATABASE -w --command "GRANT USAGE ON SCHEMA mis TO ldpreport;" &&\
+psql -U ldpadmin -h $DB_HOST -d $PG_DATABASE -w --command "GRANT USAGE ON SCHEMA folio_reporting TO ldpreport;" &&\
+psql -U ldpadmin -h $DB_HOST -d $PG_DATABASE -w --command "GRANT USAGE ON SCHEMA public TO ldpreport;" &&\
+psql -U ldpadmin -h $DB_HOST -d $PG_DATABASE -w --command "GRANT USAGE ON SCHEMA history TO ldpreport;" &&\
+psql -U ldpadmin -h $DB_HOST -d $PG_DATABASE -w --command "GRANT SELECT ON ALL TABLES IN SCHEMA mis TO ldpreport;" &&\
+psql -U ldpadmin -h $DB_HOST -d $PG_DATABASE -w --command "GRANT SELECT ON ALL TABLES IN SCHEMA folio_reporting TO ldpreport;" &&\
+psql -U ldpadmin -h $DB_HOST -d $PG_DATABASE -w --command "GRANT SELECT ON ALL TABLES IN SCHEMA public TO ldpreport;" &&\
+psql -U ldpadmin -h $DB_HOST -d $PG_DATABASE -w --command "GRANT SELECT ON ALL TABLES IN SCHEMA history TO ldpreport;" &&\
+psql -U ldpadmin -h $DB_HOST -d $PG_DATABASE -w --command "ALTER DEFAULT PRIVILEGES IN SCHEMA mis GRANT SELECT ON TABLES TO ldpreport;" &&\
+psql -U ldpadmin -h $DB_HOST -d $PG_DATABASE -w --command "ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO ldp;"
+
+echo Running item history update...
+
+# Change directories to run item history update perl script
+cd /usr/local/bin/ldp
+perl ./item_history_update.pl
 
 # After all database updates and derived table queries have completed, it is recommended to run vacuum and analyze as superuser
-psql -U postgres -h $DB_HOST -d $PG_DATABASE -w --command "VACUUM;" &&\
-psql -U postgres -h $DB_HOST -d $PG_DATABASE -w --command "ANALYZE;"
+psql -U ldpadmin -h $DB_HOST -d $PG_DATABASE -w --command "VACUUM;" &&\
+psql -U ldpadmin -h $DB_HOST -d $PG_DATABASE -w --command "ANALYZE;"
+
+echo Done!
